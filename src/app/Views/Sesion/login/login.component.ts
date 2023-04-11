a162a3d3ecab81844d8f099dbaac027c7d554207
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-11 08:21:39 -0300

    cambio de fuente de iconos, sus iconos y aplicacion de modal para registrar usuarios

--- src/app/Views/Sesion/login/login.component.ts
+++ src/app/Views/Sesion/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  nombreCtrl= new FormControl('', [Validators.required,Validators.minLength(4)]);
+  apellidoCtrl= new FormControl('', [Validators.required,Validators.minLength(4)]);
+  telefonoCtrl= new FormControl('', [Validators.required,Validators.minLength(6)]);
+  contraseniaCtrl= new FormControl('', [Validators.required,Validators.minLength(8)]);
+  confirmacionCtrl= new FormControl('', [Validators.required,Validators.minLength(8)]);
+  dniCtrl= new FormControl('', [Validators.required]);
+  correoCtrl=new FormControl('', [Validators.required,Validators.email]);
+  usuarioCtrl= new FormControl('', [Validators.required,Validators.minLength(4)]);
+
   constructor() { }
 
-  ngOnInit(): void {
+  
+
+  ngOnInit(): void { }
+
+
+
+  /* ----- Modal --- */
+  validarContrasenia(){
+
+    var usuario = {
+      Nombre: this.nombreCtrl.value + '',
+      Apellido: this.apellidoCtrl.value + '',
+      Celular: this.telefonoCtrl.value + '',
+      DNI: this.dniCtrl.value + '',
+      Nick: this.usuarioCtrl.value + '',
+      Correo: this.correoCtrl.value + '',
+      Contrasenia: this.contraseniaCtrl.value + '',
+      ID_Perfil: 3, /* usuario */
+      ID_Estado: 10,/* activo */
+    }
+
+    console.log('Usuario Registrado',usuario);
+    
+
+
   }
+  
 
 }
