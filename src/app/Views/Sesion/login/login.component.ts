d2cd95cadd6a89f01fe3bd1ceb48555fd4d12f67
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-12 16:58:38 -0300

    implementacion del metodo postUsuario en el modal de registro de usuario

--- src/app/Views/Sesion/login/login.component.ts
+++ src/app/Views/Sesion/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  FormControl, Validators } from '@angular/forms';
+import { LoginApiService } from 'src/app/Services/Login/login-api.service';
+import { RegistroApiService } from 'src/app/Services/Registro/registro-api.service';
 import { BackenApiService } from 'src/app/Services/backen-api.service';
 
 @Component({
@@ -13,26 +15,22 @@ export class LoginComponent implements OnInit {
   userLogCtrl = new FormControl('', [Validators.required,Validators.minLength(4)]);
   pasworLogCtrl = new FormControl('', [Validators.required,Validators.minLength(4)]);
 
-
-
-
   /*------ Registrar Usuario ------ */
-  nombreCtrl= new FormControl('', [Validators.required,Validators.minLength(4)]);
-  apellidoCtrl= new FormControl('', [Validators.required,Validators.minLength(4)]);
-  telefonoCtrl= new FormControl('', [Validators.required,Validators.minLength(6)]);
-  contraseniaCtrl= new FormControl('', [Validators.required,Validators.minLength(8)]);
-  confirmacionCtrl= new FormControl('', [Validators.required,Validators.minLength(8)]);
+  nombreCtrl= new FormControl('', [Validators.required]);
+  apellidoCtrl= new FormControl('', [Validators.required,]);
+  telefonoCtrl= new FormControl('', [Validators.required]);
+  contraseniaCtrl= new FormControl('', [Validators.required]);
+  confirmacionCtrl= new FormControl('', [Validators.required]);
   dniCtrl= new FormControl('', [Validators.required]);
-  correoCtrl=new FormControl('', [Validators.required,Validators.email]);
-  usuarioCtrl= new FormControl('', [Validators.required,Validators.minLength(4)]);
-
-  constructor(private service: BackenApiService ) { }
-
+  correoCtrl=new FormControl('', [Validators.required]);
+  usuarioCtrl= new FormControl('', [Validators.required]);
   
+  constructor(private service: LoginApiService, private serviceRegistro:RegistroApiService ) { }
+
+  banderaContrasenia:boolean=false; /* bandera para habilitar el boton de registrarme */
+  banderaAlerta:boolean=false;
 
   ngOnInit(): void { }
-
-
 
   /* ----- Login ----- */
 
@@ -46,6 +44,7 @@ export class LoginComponent implements OnInit {
       (data) =>{
         console.log('el usuario se logueo con exito')
         console.log(data)
+        alert('usuario logueado con el servicio de login')
       },
       (error) => {
         console.log('Hubo un problema y el usuario no se logueo con exito')
@@ -56,27 +55,48 @@ export class LoginComponent implements OnInit {
 
   }
 
-
-
-  /* ----- Modal --- */
+  /* ----- Modal Registrar usuario --- */
   validarContrasenia(){
-
-    var usuario = {
-      Nombre: this.nombreCtrl.value + '',
-      Apellido: this.apellidoCtrl.value + '',
-      Celular: this.telefonoCtrl.value + '',
-      DNI: this.dniCtrl.value + '',
-      Nick: this.usuarioCtrl.value + '',
-      Correo: this.correoCtrl.value + '',
-      Contrasenia: this.contraseniaCtrl.value + '',
-      ID_Perfil: 3, /* usuario */
-      ID_Estado: 10,/* activo */
+    var contrasenia = this.contraseniaCtrl.value + '';
+    var confirmacion = this.confirmacionCtrl.value + '';
+    if (contrasenia===confirmacion) {
+      debugger
+      this.banderaContrasenia = true; /* si las contraseñas son iguales */
+      console.log('contrasenia iguales');
+    } else if (contrasenia != confirmacion) {
+      this.banderaContrasenia = false; /* si las contraseñas no son iguales */
+      console.log('contrasenia no son iguales');
     }
+  }
 
-    console.log('Usuario Registrado',usuario);
+  registrarUsuario(){
+    debugger
+    if(this.nombreCtrl.invalid || this.apellidoCtrl.invalid || this.telefonoCtrl.invalid || this.dniCtrl.invalid|| this.usuarioCtrl.invalid || this.correoCtrl.invalid || this.contraseniaCtrl.invalid || this.confirmacionCtrl.invalid || this.banderaContrasenia==false){
+      alert('faltan datos por agregar')
+    }else{
+
+      var usuario = {
+        Nombre: this.nombreCtrl.value + '',
+        Apellido: this.apellidoCtrl.value + '',
+        Celular: this.telefonoCtrl.value + '',
+        DNI: this.dniCtrl.value + '',
+        Nick: this.usuarioCtrl.value + '',
+        Correo: this.correoCtrl.value + '',
+        Contrasenia: this.contraseniaCtrl.value + '',
+        ID_Perfil: 3, /* usuario */
+        ID_Estado: 10,/* activo */
+      }
+
+      this.serviceRegistro.postRegistrarUsuario(usuario).subscribe(
+        (data) => {
+          alert('Usuario registrado: ' + data)
+        },
+        (error) =>{
+          alert('ocurrio un error al registarr el usuario: ' + error)
+        }
+      )
+    }
     
-
-
   }
   
 
