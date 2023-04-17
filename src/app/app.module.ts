4a4ad95bb1a9d60c0fbf98b281ee0cc8db8e2162
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-16 23:05:21 -0300

    importacion del componente menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Views/home/home.component';
 import { LoginComponent } from './Views/Sesion/login/login.component';
 import { RegistroComponent } from './Views/Sesion/registro/registro.component';
+import { MenuComponent } from './Views/Estructura/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
