0b2658b00b899da5b5e59a15372fac5a4c759304
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-17 16:34:39 -0300

    prueba de alertas

---
+++ src/assets/js/notificacion-registro.js
@@ -0,0 +1,8 @@
+
+
+function notificacionRegistro(){
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+      return new bootstrap.Toast(toastEl, option)
+    })
+}
