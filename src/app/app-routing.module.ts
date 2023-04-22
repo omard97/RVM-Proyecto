14db97e12f2562734733080ef984ffc2b9641fc8
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-22 20:46:04 -0300

    Rutas hijas perfil y dashboard creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { HomeComponent } from './Views/home/home.component';
 import { LoginComponent } from './Views/Sesion/login/login.component';
 import { RegistroComponent } from './Views/Sesion/registro/registro.component';
 import { MenuComponent } from './Views/Estructura/menu/menu.component';
+import { PerfilComponent } from './Views/Usuario/perfil/perfil.component';
+import { DashboardComponent } from './Views/Dashboard/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: '',component:HomeComponent},
   {path: 'login',component:LoginComponent},
   {path: 'registro', component:RegistroComponent},
-  {path: 'menu/:id', component:MenuComponent}
+
+  /*estando en el meu luego de iniciar sesion  */
+  {path: 'menu/:id', component:MenuComponent,
+    children: [
+      {path: 'perfil',component:PerfilComponent},
+      {path: 'dashboard',component:DashboardComponent},
+    ] 
+  }
   
 ];
 
