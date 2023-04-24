62eb933d9d0740aba201898439576ad52b5df826
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-04-24 08:29:16 -0300

    Estilo del menu y funcionalidad al traer los datos des usuario

--- src/app/Views/Usuario/perfil/perfil.component.ts
+++ src/app/Views/Usuario/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PerfilApiService } from 'src/app/Services/Perfil/perfil-api.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+
+  idUsuario:any;
+  rutaURL:any
+  datosPerfil:any;
+
+  constructor(private servicePeril:PerfilApiService,private _route: ActivatedRoute) { 
+
+    this.rutaURL = window.location.pathname.split('/');
+    this.idUsuario = this.rutaURL[2];
+  
+    this.getDatosPerfil();
+  }
 
   ngOnInit(): void {
+  }
+
+
+  getDatosPerfil(){
+    
+    this.servicePeril.getdatosPerfil(Number(this.idUsuario)).subscribe(
+      (data)=>{
+        console.log('datos del perfil logueado: ',data)
+        this.datosPerfil=data;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
   }
 
 }
